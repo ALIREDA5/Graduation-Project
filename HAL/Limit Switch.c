31496411c0004a811ce15e7c17fccadda4759b72
Salma Bakr <124618659+SalmaBakr22@users.noreply.github.com>
2024-01-30 20:31:06 +0200

    Update Limit Switch.c

--- HAL/Limit Switch.c
+++ HAL/Limit Switch.c
@@ -7,20 +7,20 @@
 
 
 		/*----------------- Libraries and Definitions ------------*/
-
+#include "DIO.h"
 #include "Limit Switch.h"
 
 		/*--------------------- Global Variables ------------------*/
 
-static uint16 x;
+static uint8 u8LSValue;
 
 		
 // PIN connected to the limit switch should read 1 when switch is pressed in case of NO 
 // remember to connect 10k resistor between the pin and limit switch
 
 		/*------------------------- Function Definitions -----------------------------*/
-uint8 LS_Read(uint8 p)
+uint8 LS_Read(uint8 u8pinCpy)
 {
-	 DIO_enuReadPinValue(p,x);
-	return x;
+	 DIO_enuReadPinValue(u8pinCpy,&u8LSValue);
+	return u8LSValue;
 }
