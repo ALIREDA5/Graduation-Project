803c098864d9644edb600e937a8966add02e74c8
Salma Bakr <124618659+SalmaBakr22@users.noreply.github.com>
2024-02-11 13:43:45 +0200

    Update STD_Types.h

--- Lib/STD_Types.h
+++ Lib/STD_Types.h
@@ -1,30 +1,30 @@
-/******************************************/
-/* Desc    : Standard types header file   */
-/* Author  : Salma Bakr 			      */
-/* Date    : 4 Aug 2023 				  */
-/* Version : 1.0v                         */
-/******************************************/
+/**********************************************************************************************/
+/** Desc : standard types header file                                                                                     */
+/** Author : Salma Bakr                                                                                                                        */
+/** Date : 4 Aug 2023                                                                                                            */
+/** version : 1.0v                                                                                                                 */
+/****************************************************************************************/
 
 #ifndef STD_TYPES_H
 #define STD_TYPES_H
-
+#define NULL                     ((void *)0)
 typedef void (*pf)(void);
 
-typedef signed char              sint8  ;
-typedef unsigned char            uint8  ;
-typedef signed short int         sint16 ;
-typedef unsigned short int       uint16 ;
-typedef signed long int          sint32 ;
-typedef unsigned long int        uint32 ;
-typedef float                    float32;
-typedef double                   float64;
+typedef signed char                 sint8;
+typedef unsigned char               uint8;
+typedef signed short int            sint16;
+typedef unsigned short int          uint16;
+typedef signed long int             sint32;
+typedef unsigned long int           uint32;
+typedef float                       float32;
+typedef double                      float64;
 
-typedef enum 
-{
+typedef enum{
 	E_OK,
 	E_NOK,
-	PARM_POT_OF_RANGE,
-	PTR_IS_NULL
+	OUT_OF_RANG,
+	IS_NULL_POINTER
+	
 }tenuErrorStatus;
 
-#endif   /*End Guard*/
+#endif /** End Gaurd */
