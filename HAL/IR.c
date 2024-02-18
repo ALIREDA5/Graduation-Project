4feb83c2b8c61f8470c3eab88e589ab18dff1c01
Salma Bakr <124618659+SalmaBakr22@users.noreply.github.com>
2024-02-18 22:55:10 +0200

    Add files via upload

---
+++ HAL/IR.c
@@ -0,0 +1,26 @@
+/*
+ * IR_Sensor.c
+ *
+ * Created: 10/28/2023 11:36:31 PM
+ *  Author: Ali Reda
+ */ 
+
+		/*----------------- Libraries and Definitions ------------*/
+#include "STD_Types.h"
+#include "DIO.h"	
+#include "IR.h"
+		/*--------------------- Global Variables ------------------*/
+
+static uint8 IRreadVlue;
+		
+// pin will be High as any thing within the Detection Distance
+// The potentiometer R5 is used to adjust how sensitive the sensor is. 
+// You can use it to adjust the distance from the object at which the sensor detects it.
+
+		/*------------------------- Function Definitions -----------------------------*/
+		
+uint8 IR_ReadPinValue(void)
+{
+	DIO_enuReadPinValue(IRPin , &IRreadVlue);
+	return IRreadVlue;
+}	
---
+++ HAL/IR.h
@@ -0,0 +1,17 @@
+/*
+ * IR_Sensor.h
+ *
+ * Created: 10/28/2023 11:36:46 PM
+ *  Author: Ali Reda
+ */ 
+
+
+#ifndef IR SENSOR_H_
+#define IR SENSOR_H_
+
+		/*--------------------- Functions Prototypes ---------------------------*/
+#define IRPin 0
+		
+uint8 IR_ReadPinValue(void);
+
+#endif /* IR SENSOR_H_ */
