eba00c31c57009ed8559448c42c3a8d3cd237f78
Salma Bakr <124618659+SalmaBakr22@users.noreply.github.com>
2024-02-29 12:25:04 +0200

    Update LDR.h

--- Sensors/LDR.h
+++ Sensors/LDR.h
@@ -1,21 +1,9 @@
-/*
- * LDR.h
- *
- * Created: 10/29/2023 1:02:29 AM
- *  Author: Ali Reda
- */ 
-
-
 #ifndef LDR_H_
 #define LDR_H_
 
-		/*----------------- Libraries and Definitions ------------*/
-
-#include "../MCAL/ADC.h"
-		
-		/*--------------------- Functions Prototypes ---------------------------*/
-
-uint8_t LDR_Read(uint8 cha);
+	/*--------------------- LDR Functions Prototypes ---------------------------*/
+	
+uint8 LDR_Read(void);
 
 
-#endif /* LDR_H_ */
+#endif LDR_H_
